04d8c27050f55c606ae64cd584478e488f192953
CrashTex <176623885+CrashTex@users.noreply.github.com>
2026-01-19 14:01:33 +0100

    Register response code

--- src/main/java/com/helha/thelostgrimoire/controllers/users/UserCommandController.java
+++ src/main/java/com/helha/thelostgrimoire/controllers/users/UserCommandController.java
@@ -6,7 +6,13 @@ import com.helha.thelostgrimoire.application.users.command.login.LoginInput;
 import com.helha.thelostgrimoire.application.users.command.register.RegisterInput;
 import com.helha.thelostgrimoire.application.users.command.register.RegisterOutput;
 import com.helha.thelostgrimoire.domain.User;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,12 +41,28 @@ public class UserCommandController {
     }
 
     @PostMapping("/register")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    headers = @Header(
+                            name = "User Register",
+                            description = "User created"
+                    )
+            ),
+            @ApiResponse(responseCode = "400",
+                    content = @Content(schema = @Schema(implementation = org.springframework.http.ProblemDetail.class)))
+    })
     public ResponseEntity<RegisterOutput> register(@Valid @RequestBody RegisterInput input) {
         RegisterOutput output = usersCommandProcessor.registerHandler.handle(input);
 
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(output.id)
+                .toUri();
 
-
-        return ResponseEntity.ok(output);
+        return ResponseEntity
+                .created(location)
+                .body(output);
     }
 }
 
